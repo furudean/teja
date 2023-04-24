34d2810bd0432c8abfa6d15fcf8ced8ecd923b7e
Merilynn Bandy <31383109+furudean@users.noreply.github.com>
2023-04-23 18:13:44 -0600

    ok

--- prisma/migrations/20230424000157_position/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `position` to the `Client` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Client" ADD COLUMN     "position" TEXT NOT NULL;
---
+++ prisma/migrations/20230424001231_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Client" ADD COLUMN     "position" TEXT NOT NULL DEFAULT '';
